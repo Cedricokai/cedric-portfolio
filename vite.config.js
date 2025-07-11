c8ab2e88d676e747f0d71a7b2640acf9bb843ea3
Cedricokai <88492207+Cedricokai@users.noreply.github.com>
2025-07-11 09:16:52 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Cedric-portfolio/',
+  base: '/cedric-portfolio/',
   css: {
     postcss: {
       plugins: [
